37ee48adac04cc28007a70781bbb597abaeca8e8
Umar Sear <umasear@cisco.com>
2015-04-03 20:16:51 -0700

    Changed get_site_id to get site_details

---
+++ main.py
@@ -0,0 +1,18 @@
+__author__ = 'Umar Sear'
+import sys
+from db_sqlite3 import get_db_row_count, get_site_details
+from solar_edge import get_energy_values
+from datetime import date
+
+
+def main():
+    row_count = get_db_row_count("/Users/umasear/code/solar-energy-notification/data/solar_notification.db","sites");
+
+    for x in range(1,row_count+1):
+        site_details = get_site_details("/Users/umasear/code/solar-energy-notification/data/solar_notification.db",x)
+        kwhr = get_energy_values(site_details[4], site_details[0], date.today())
+        print("{}'s solar system produced {} KWhr of electricity".format(site_details[2], kwhr))
+
+
+if __name__ == '__main__':
+    sys.exit(main())
