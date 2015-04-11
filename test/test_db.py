a0b3cb9efccf82e6a4266f208874677e22af72d3
Umar Sear <umasear@cisco.com>
2015-04-10 23:12:03 -0700

    changed database path to relative in db test

--- test/test_db.py
+++ test/test_db.py
@@ -10,7 +10,7 @@ parser = ConfigParser()
 if len(sys.argv) > 1:
     parser.read(sys.argv[1])
 else:
-    parser.read('solar_notification.ini')
+    parser.read('../solar_notification.ini')
 
 database_name = parser.get('database', 'dbfilename')
 
@@ -18,7 +18,7 @@ database_name = parser.get('database', 'dbfilename')
 class DatabaseTestCase(unittest.TestCase):
 
     def test_get_db_row_count(self):
-        self.assertGreaterEqual(get_db_row_count(database_name, "sites"),0)
+        self.assertGreaterEqual(get_db_row_count("../{}".format(database_name), "sites"),0)
 
 if __name__ == '__main__':
     unittest.main()
